febc3e2093141954f63e7cede61b619c7534e8f8
Antonio Okoro <cheezytony1@gmail.com>
2026-04-08 13:30:22 +0100

    fix: reorganize imports and enhance TypeOrmModule configuration in app.module.ts

--- votu-backend/src/app.module.ts
+++ votu-backend/src/app.module.ts
@@ -1,22 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { ScheduleModule } from '@nestjs/schedule';
 import { BullModule } from '@nestjs/bullmq';
-import { APP_FILTER, APP_INTERCEPTOR, APP_GUARD } from '@nestjs/core';
-import { ClassSerializerInterceptor } from '@nestjs/common';
-import { ThrottlerGuard } from '@nestjs/throttler';
-import { validationSchema } from './config/env.validation';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { RedisModule } from './redis/redis.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { PollsModule } from './polls/polls.module';
-import { VotesModule } from './votes/votes.module';
-import { NotificationsModule } from './notifications/notifications.module';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { validationSchema } from './config/env.validation';
+import { NotificationsModule } from './notifications/notifications.module';
+import { PollsModule } from './polls/polls.module';
+import { RedisModule } from './redis/redis.module';
+import { UsersModule } from './users/users.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
   imports: [
@@ -26,12 +24,20 @@ import { AppService } from './app.service';
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        autoLoadEntities: true,
-        synchronize: false,
-      }),
+      useFactory: (configService: ConfigService) => {
+        console.log(
+          'configService.get<string>(DATABASE_URL)',
+          configService.get<string>('DATABASE_URL'),
+        );
+
+        return {
+          type: 'postgres',
+          url: configService.get<string>('DATABASE_URL'),
+          autoLoadEntities: true,
+          synchronize: false,
+          ssl: { rejectUnauthorized: false },
+        };
+      },
     }),
     ThrottlerModule.forRootAsync({
       inject: [ConfigService],
